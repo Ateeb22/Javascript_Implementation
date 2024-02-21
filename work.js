b8aed4709ce49dbdfc2aeecd6e8d7f722c7ce6e8
Ateeb Sheikh <Ateeb.Sheikh@CSPKAM00000843.mLogica.com>
2024-02-21 11:08:01 +0500

    new work

--- work.js
+++ work.js
@@ -1,3 +1,9 @@
 
       fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json())
       .then(json => console.log(json))
+      
+      
+      
+      
+      // .then(response => response.json())
+      // .then(json => console.log(json))
